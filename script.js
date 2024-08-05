b2f76a989dc17d9b02c66f961b91624df8009626
DDD2308 <141920527+DDD2308@users.noreply.github.com>
2024-08-05 19:17:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,20 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
         nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
         responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
+             {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
             }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
         ]
     });   
 });
